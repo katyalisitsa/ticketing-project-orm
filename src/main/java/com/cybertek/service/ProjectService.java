f943ec7421aac273ebc5afc8232a01756cbf4f27
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-02 10:26:44 -0500

    initial commit

---
+++ src/main/java/com/cybertek/service/ProjectService.java
@@ -0,0 +1,8 @@
+package com.cybertek.service;
+
+import com.cybertek.dto.ProjectDTO;
+
+public interface ProjectService extends CrudService<ProjectDTO,String>  {
+
+    void complete(ProjectDTO project);
+}
