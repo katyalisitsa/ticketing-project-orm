52a882b8d4ec48623b2fa6d0334452586a866ec3
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-05 21:25:01 -0500

    Mappers updates

--- src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
+++ src/main/java/com/cybertek/implementation/ProjectServiceImpl.java
@@ -4,19 +4,24 @@ import com.cybertek.dto.ProjectDTO;
 import com.cybertek.entity.Project;
 import com.cybertek.enums.Status;
 import com.cybertek.mapper.ProjectMapper;
+import com.cybertek.mapper.UserMapper;
 import com.cybertek.repository.ProjectRepository;
 import com.cybertek.service.ProjectService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProjectServiceImpl implements ProjectService {
 
     private ProjectMapper projectMapper;
     private ProjectRepository projectRepository;
+    private UserMapper userMapper;
 
-    public ProjectServiceImpl(ProjectMapper projectMapper, ProjectRepository projectRepository) {
+    public ProjectServiceImpl(ProjectMapper projectMapper, ProjectRepository projectRepository, UserMapper userMapper) {
         this.projectMapper = projectMapper;
         this.projectRepository = projectRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
@@ -34,6 +39,7 @@ public class ProjectServiceImpl implements ProjectService {
 
         dto.setProjectStatus(Status.OPEN);
         Project obj = projectMapper.convertToEntity(dto);
+        obj.setAssignedManager(userMapper.convertToEntity(dto.getAssignedManager()));
         Project project = projectRepository.save(obj);
     }
 
